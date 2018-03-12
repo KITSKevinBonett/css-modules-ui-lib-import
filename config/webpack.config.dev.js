8d33b82302097f201c63c06524c8fec948933a43
KITSKevinBonett <kevin.bonett@kingfisher.com>
2018-03-12 15:59:04 +0000

    added CSS Modules support for '.css' files

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -85,7 +85,7 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -116,7 +116,7 @@ module.exports = {
             options: {
               formatter: eslintFormatter,
               eslintPath: require.resolve('eslint'),
-              
+
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -145,7 +145,7 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              
+
               // This is a feature of `babel-loader` for webpack (not Babel itself).
               // It enables caching results in ./node_modules/.cache/babel-loader/
               // directory for faster rebuilds.
@@ -201,7 +201,11 @@ module.exports = {
               {
                 loader: require.resolve('css-loader'),
                 options: {
+                  minimize: true,
+                  sourceMap: true,
+                  modules: true,
                   importLoaders: 1,
+                  localIdentName: '[name]___[local]___[hash:base64:5]'
                 },
               },
               {
